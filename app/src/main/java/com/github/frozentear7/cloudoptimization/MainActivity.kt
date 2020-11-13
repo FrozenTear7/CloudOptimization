9478d5cb8d4529264fa217d503b1cacd86785fcb
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-11-13 16:52:39 +0100

    Basic layout to test the battery without charging

--- app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
+++ app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.os.BatteryManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
 
 private const val TAG = "MainActivity"
 
@@ -13,21 +16,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val chargeCounterValueTextView = findViewById<TextView>(R.id.chargeCounterValueTextView)
+        val capacityValueTextView = findViewById<TextView>(R.id.capacityValueTextView)
+        val currentTimeTextView = findViewById<TextView>(R.id.currentTimeTextView)
+
         val mBatteryManager: BatteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        val scope = CoroutineScope(Dispatchers.Default)
 
-        val chargeCounter: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
-        Log.i(TAG, "Remaining battery capacity = $chargeCounter uAh")
+        scope.launch {
+            withContext(Dispatchers.Main) {
+                while (true) {
+                    val current = LocalDateTime.now()
+                    println("Current Date and Time is: $current")
+                    currentTimeTextView.text = current.toString()
 
-        val currentNow: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
-        Log.i(TAG, "Instantaneous battery current $currentNow uA")
+                    val chargeCounter: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
+                    Log.i(TAG, "Remaining battery capacity = $chargeCounter uAh")
+                    chargeCounterValueTextView.text = chargeCounter.toString()
 
-        val currentAvg: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
-        Log.i(TAG, "Average battery current = $currentAvg uA")
+//                    val currentNow: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+//                    Log.i(TAG, "Instantaneous battery current $currentNow uA")
 
-        val capacity: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-        Log.i(TAG, "Remaining battery capacity = $capacity %")
+//                    val currentAvg: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
+//                    Log.i(TAG, "Average battery current = $currentAvg uA")
 
-        val energyCounter: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
-        Log.i(TAG, "Remaining energy = $energyCounter nWh")
+                    val capacity: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+                    Log.i(TAG, "Remaining battery capacity = $capacity %")
+                    capacityValueTextView.text = capacity.toString()
+
+//                    val energyCounter: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
+//                    Log.i(TAG, "Remaining energy = $energyCounter nWh")
+
+                    delay(10000)
+                }
+            }
+        }
     }
 }
