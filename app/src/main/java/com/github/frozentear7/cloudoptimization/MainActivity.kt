7d337cf8c118c9e8fb76907618e9787910381da6
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-11-16 15:23:35 +0100

    Working OCR without mkdir

--- app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
+++ app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.github.frozentear7.cloudoptimization
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.BatteryManager
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.coroutines.*
+import java.io.*
 import java.time.LocalDateTime
 
+
 private const val TAG = "MainActivity"
+private var DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess"
+private const val lang = "eng"
+private const val PHOTO_TAKEN: String = "photo_taken"
 
 class MainActivity : AppCompatActivity() {
+    private var path: String = "$DATA_PATH/ocr.jpg";
+    private var photoTaken: Boolean = false
+
+    private lateinit var ocrResultEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,12 +43,55 @@ class MainActivity : AppCompatActivity() {
         val chargeCounterChangeValueTextView =
             findViewById<TextView>(R.id.chargeCounterChangeValueTextView)
         val currentTimeTextView = findViewById<TextView>(R.id.currentTimeTextView)
+        val takePhotoButton = findViewById<Button>(R.id.takePhotoButton)
+
+        ocrResultEditText = findViewById(R.id.ocrResultEditText)
 
         val mBatteryManager: BatteryManager =
             getSystemService(Context.BATTERY_SERVICE) as BatteryManager
         val scope = CoroutineScope(Dispatchers.Default)
         var prevChargeCounter = -1L
         var totalEnergyUsed = 0L
+
+        val paths = arrayOf(DATA_PATH, "$DATA_PATH/tessdata/")
+
+        for (path in paths) {
+            val dir = File(path)
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory $path on sdcard failed")
+                    return
+                } else {
+                    Log.v(TAG, "Created directory $path on sdcard")
+                }
+            }
+        }
+
+        if (!File("$DATA_PATH/tessdata/$lang.traineddata").exists()) {
+            try {
+                val assetManager = assets
+                val `in`: InputStream = assetManager.open("tessdata/$lang.traineddata")
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                val out: OutputStream = FileOutputStream(
+                    DATA_PATH
+                            + "tessdata/" + lang + ".traineddata"
+                )
+
+                // Transfer bytes from in to out
+                val buf = ByteArray(1024)
+                var len: Int
+                //while ((lenf = gin.read(buff)) > 0) {
+                while (`in`.read(buf).also { len = it } > 0) {
+                    out.write(buf, 0, len)
+                }
+                `in`.close()
+                //gin.close();
+                out.close()
+                Log.v(TAG, "Copied $lang traineddata")
+            } catch (e: IOException) {
+                Log.e(TAG, "Was unable to copy $lang traineddata $e")
+            }
+        }
 
         scope.launch {
             withContext(Dispatchers.Main) {
@@ -64,6 +129,112 @@ class MainActivity : AppCompatActivity() {
                     delay(10000)
                 }
             }
+        }
+
+        takePhotoButton.setOnClickListener {
+            Log.v(TAG, "Starting Camera app")
+            startCameraActivity()
+        }
+    }
+
+    private fun startCameraActivity() {
+        val file = File(path)
+//        val outputFileUri = Uri.fromFile(file)
+        val outputFileUri = FileProvider.getUriForFile(
+            applicationContext,
+            BuildConfig.APPLICATION_ID + ".provider",
+            file
+        )
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+        startActivityForResult(intent, 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i(TAG, "resultCode: $resultCode")
+        if (resultCode == -1) {
+            onPhotoTaken()
+        } else {
+            Log.v(TAG, "User cancelled")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(PHOTO_TAKEN, photoTaken)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i(TAG, "onRestoreInstanceState()")
+        if (savedInstanceState.getBoolean(PHOTO_TAKEN)) {
+            onPhotoTaken()
+        }
+    }
+
+    private fun onPhotoTaken() {
+        photoTaken = true
+        val options = BitmapFactory.Options()
+        options.inSampleSize = 4
+        var bitmap = BitmapFactory.decodeFile(path, options)
+        try {
+            val exif = ExifInterface(path)
+            val exifOrientation: Int = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
+            Log.v(TAG, "Orient: $exifOrientation")
+            var rotate = 0
+            when (exifOrientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotate = 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotate = 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotate = 270
+            }
+            Log.v(TAG, "Rotation: $rotate")
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                val w = bitmap.width
+                val h = bitmap.height
+
+                // Setting pre rotate
+                val mtx = Matrix()
+                mtx.preRotate(rotate.toFloat())
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false)
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        } catch (e: IOException) {
+            Log.e(TAG, "Couldn't correct orientation: $e")
+        }
+
+        // _image.setImageBitmap( bitmap );
+        Log.v(TAG, "Before baseApi")
+        val baseApi = TessBaseAPI()
+        baseApi.setDebug(true)
+        baseApi.init(DATA_PATH, lang)
+        baseApi.setImage(bitmap)
+        var recognizedText = baseApi.utF8Text
+        baseApi.end()
+
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+        Log.v(TAG, "OCRED TEXT: $recognizedText")
+        if (lang.equals("eng", ignoreCase = true)) {
+            recognizedText = recognizedText.replace("[^a-zA-Z0-9]+".toRegex(), " ")
+        }
+        recognizedText = recognizedText.trim { it <= ' ' }
+        if (recognizedText.isNotEmpty()) {
+            ocrResultEditText.setText(
+                if (ocrResultEditText.text.toString()
+                        .isEmpty()
+                ) recognizedText else ocrResultEditText.text.toString() + " " + recognizedText
+            )
+            ocrResultEditText.setSelection(ocrResultEditText.text.toString().length)
         }
     }
 }
