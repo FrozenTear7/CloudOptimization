81f537a2d044c199a8237691986a7a53da5a77ae
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-11-16 17:50:19 +0100

    Refactor code into proper functions

--- app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
+++ app/src/main/java/com/github/frozentear7/cloudoptimization/MainActivity.kt
@@ -25,31 +25,38 @@ import java.time.LocalDateTime
 private const val TAG = "MainActivity"
 private var DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess"
 private const val lang = "eng"
+private const val pdfFilename = "Raport projektu - CEA_Bus_Klemens_Mendroch.pdf"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pageImage: Bitmap
     private lateinit var root: File
+
+    private lateinit var chargeCounterValueTextView: TextView
+    private lateinit var capacityValueTextView: TextView
+    private lateinit var chargeCounterChangeValueTextView: TextView
+    private lateinit var currentTimeTextView: TextView
+    private lateinit var takePhotoButton: Button
     private lateinit var ocrResultEditText: EditText
-    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private lateinit var mBatteryManager: BatteryManager
+    var prevChargeCounter = -1L
+    var totalEnergyUsed = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val chargeCounterValueTextView = findViewById<TextView>(R.id.chargeCounterValueTextView)
-        val capacityValueTextView = findViewById<TextView>(R.id.capacityValueTextView)
-        val chargeCounterChangeValueTextView =
-            findViewById<TextView>(R.id.chargeCounterChangeValueTextView)
-        val currentTimeTextView = findViewById<TextView>(R.id.currentTimeTextView)
-        val takePhotoButton = findViewById<Button>(R.id.takePhotoButton)
+        mBatteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
 
+        chargeCounterValueTextView = findViewById(R.id.chargeCounterValueTextView)
+        capacityValueTextView = findViewById(R.id.capacityValueTextView)
+        chargeCounterChangeValueTextView =
+            findViewById(R.id.chargeCounterChangeValueTextView)
+        currentTimeTextView = findViewById(R.id.currentTimeTextView)
+        takePhotoButton = findViewById(R.id.takePhotoButton)
         ocrResultEditText = findViewById(R.id.ocrResultEditText)
 
-        val mBatteryManager: BatteryManager =
-            getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-        var prevChargeCounter = -1L
-        var totalEnergyUsed = 0L
-
+        // Create tessdata trained data directories on the phone
         val paths = arrayOf(DATA_PATH, "$DATA_PATH/tessdata/")
 
         for (path in paths) {
@@ -68,7 +75,6 @@ class MainActivity : AppCompatActivity() {
             try {
                 val assetManager = assets
                 val `in`: InputStream = assetManager.open("tessdata/$lang.traineddata")
-                //GZIPInputStream gin = new GZIPInputStream(in);
                 val out: OutputStream = FileOutputStream(
                     DATA_PATH
                             + "tessdata/" + lang + ".traineddata"
@@ -77,12 +83,10 @@ class MainActivity : AppCompatActivity() {
                 // Transfer bytes from in to out
                 val buf = ByteArray(1024)
                 var len: Int
-                //while ((lenf = gin.read(buff)) > 0) {
                 while (`in`.read(buf).also { len = it } > 0) {
                     out.write(buf, 0, len)
                 }
                 `in`.close()
-                //gin.close();
                 out.close()
                 Log.v(TAG, "Copied $lang traineddata")
             } catch (e: IOException) {
@@ -91,138 +95,160 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Setup energy counters
+        printEnergyStatus()
+
+        takePhotoButton.setOnClickListener {
+            Log.v(TAG, "Starting OCR")
+            runPdfOCR()
+        }
+    }
+
+    private fun printEnergyStatus() {
+        val scope = CoroutineScope(Dispatchers.Default)
         scope.launch {
             withContext(Dispatchers.Main) {
                 while (true) {
                     val current = LocalDateTime.now()
-                    println("Current Date and Time is: $current")
-                    currentTimeTextView.text = current.toString()
-
                     val chargeCounter: Long =
                         mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)
-                    Log.i(TAG, "Remaining battery capacity = $chargeCounter uAh")
-                    chargeCounterValueTextView.text = chargeCounter.toString()
-
-//                    val currentNow: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
-//                    Log.i(TAG, "Instantaneous battery current $currentNow uA")
-
-//                    val currentAvg: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
-//                    Log.i(TAG, "Average battery current = $currentAvg uA")
-
+                    val currentNow: Long =
+                        mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+                    val currentAvg: Long =
+                        mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE)
                     val capacity: Long =
                         mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-                    Log.i(TAG, "Remaining battery capacity = $capacity %")
-                    capacityValueTextView.text = capacity.toString()
+                    val energyCounter: Long =
+                        mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
 
                     if (prevChargeCounter != -1L)
                         totalEnergyUsed += prevChargeCounter - chargeCounter
                     prevChargeCounter = chargeCounter
 
+                    Log.i(TAG, "Current Date and Time is: $current")
+                    Log.i(TAG, "Remaining battery capacity = $chargeCounter uAh")
+                    Log.i(TAG, "Instantaneous battery current $currentNow uA")
+                    Log.i(TAG, "Average battery current = $currentAvg uA")
+                    Log.i(TAG, "Remaining battery capacity = $capacity %")
                     Log.i(TAG, "Total energy used = $totalEnergyUsed uAh")
-                    chargeCounterChangeValueTextView.text = totalEnergyUsed.toString()
+                    Log.i(TAG, "Remaining energy = $energyCounter nWh")
 
-//                    val energyCounter: Long = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_ENERGY_COUNTER)
-//                    Log.i(TAG, "Remaining energy = $energyCounter nWh")
+                    currentTimeTextView.text = current.toString()
+                    chargeCounterValueTextView.text = chargeCounter.toString()
+                    capacityValueTextView.text = capacity.toString()
+                    chargeCounterChangeValueTextView.text = totalEnergyUsed.toString()
 
                     delay(10000)
                 }
             }
         }
-
-        takePhotoButton.setOnClickListener {
-            Log.v(TAG, "Starting Camera app")
-            runPdfOCR()
-        }
     }
 
     private fun runPdfOCR() {
+        ocrResultEditText.setText("")
+        root =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+        val baseApi = TessBaseAPI()
+        baseApi.setDebug(true)
+        baseApi.init(DATA_PATH, lang)
+
         PDFBoxResourceLoader.init(applicationContext)
 
-        GlobalScope.launch {
-            // Render the page and save it to an image file
-            try {
-                // Load in an already created PDF
-                val document: PDDocument =
-                    PDDocument.load(assets.open("Raport projektu - CEA_Bus_Klemens_Mendroch.pdf"))
-                // Create a renderer for the document
-                val renderer = PDFRenderer(document)
-                // Render the image to an RGB Bitmap
-                pageImage = renderer.renderImage(0, 1f, Bitmap.Config.RGB_565)
+        var ocrResult = ""
+
+        val document: PDDocument = PDDocument.load(assets.open(pdfFilename))
+        val renderer = PDFRenderer(document)
+
+        try {
+            // Render the image to an RGB Bitmap
+            for (i in 0 until document.numberOfPages) {
+                pageImage = renderer.renderImage(i, 1f, Bitmap.Config.RGB_565)
 
                 // Save the render result to an image
-                root =
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-                val path = root.absolutePath + "/render.jpg"
-                val renderFile = File(path)
-                val fileOut = FileOutputStream(renderFile)
-                pageImage.compress(Bitmap.CompressFormat.JPEG, 100, fileOut)
-                fileOut.close()
+                val path = root.absolutePath + "/render$i.jpg"
+                saveResultImage(pageImage, path)
+                val processedImage = processImageForOCR(path)
 
-                val options = BitmapFactory.Options()
-                options.inSampleSize = 4
-                var bitmap = BitmapFactory.decodeFile(path, options)
-                try {
-                    val exif = ExifInterface(path)
-                    val exifOrientation: Int = exif.getAttributeInt(
-                        ExifInterface.TAG_ORIENTATION,
-                        ExifInterface.ORIENTATION_NORMAL
-                    )
-                    Log.v(TAG, "Orient: $exifOrientation")
-                    var rotate = 0
-                    when (exifOrientation) {
-                        ExifInterface.ORIENTATION_ROTATE_90 -> rotate = 90
-                        ExifInterface.ORIENTATION_ROTATE_180 -> rotate = 180
-                        ExifInterface.ORIENTATION_ROTATE_270 -> rotate = 270
-                    }
-                    Log.v(TAG, "Rotation: $rotate")
-                    if (rotate != 0) {
-
-                        // Getting width & height of the given image.
-                        val w = bitmap.width
-                        val h = bitmap.height
-
-                        // Setting pre rotate
-                        val mtx = Matrix()
-                        mtx.preRotate(rotate.toFloat())
-
-                        // Rotating Bitmap
-                        bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false)
-                    }
-
-                    // Convert to ARGB_8888, required by tess
-                    bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-                } catch (e: IOException) {
-                    Log.e(TAG, "Couldn't correct orientation: $e")
+                if (processedImage != null) {
+                    Log.v(TAG, "Call OCR service")
+                    baseApi.setImage(processedImage)
+                    ocrResult += baseApi.utF8Text + "\n"
                 }
-
-                // _image.setImageBitmap( bitmap );
-                Log.v(TAG, "Before baseApi")
-                val baseApi = TessBaseAPI()
-                baseApi.setDebug(true)
-                baseApi.init(DATA_PATH, lang)
-                baseApi.setImage(bitmap)
-                var recognizedText = baseApi.utF8Text
-                baseApi.end()
-
-                // You now have the text in recognizedText var, you can do anything with it.
-                // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-                // so that garbage doesn't make it to the display.
-                Log.v(TAG, "OCRED TEXT: $recognizedText")
-                if (lang.equals("eng", ignoreCase = true)) {
-                    recognizedText = recognizedText.replace("[^a-zA-Z0-9]+".toRegex(), " ")
-                }
-                recognizedText = recognizedText.trim { it <= ' ' }
-                if (recognizedText.isNotEmpty()) {
-                    ocrResultEditText.setText(
-                        if (ocrResultEditText.text.toString()
-                                .isEmpty()
-                        ) recognizedText else ocrResultEditText.text.toString() + " " + recognizedText
-                    )
-                    ocrResultEditText.setSelection(ocrResultEditText.text.toString().length)
-                }
-            } catch (e: IOException) {
-                Log.e(TAG, "Exception thrown while rendering file", e)
             }
+        } catch (e: IOException) {
+            Log.e(TAG, "Exception thrown while rendering file", e)
+        }
+
+        renderOCRResult(ocrResult)
+        baseApi.end()
+    }
+
+    private fun saveResultImage(pageImage: Bitmap, path: String) {
+        val renderFile = File(path)
+        val fileOut = FileOutputStream(renderFile)
+        pageImage.compress(Bitmap.CompressFormat.JPEG, 100, fileOut)
+        fileOut.close()
+    }
+
+    private fun processImageForOCR(path: String): Bitmap? {
+        val options = BitmapFactory.Options()
+        options.inSampleSize = 4
+
+        var bitmap = BitmapFactory.decodeFile(path, options)
+        try {
+            val exif = ExifInterface(path)
+            val exifOrientation: Int = exif.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
+            Log.v(TAG, "Orient: $exifOrientation")
+            var rotate = 0
+            when (exifOrientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotate = 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotate = 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotate = 270
+            }
+            Log.v(TAG, "Rotation: $rotate")
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                val w = bitmap.width
+                val h = bitmap.height
+
+                // Setting pre rotate
+                val mtx = Matrix()
+                mtx.preRotate(rotate.toFloat())
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false)
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+            return bitmap
+        } catch (e: IOException) {
+            Log.e(TAG, "Couldn't correct orientation: $e")
+            return null
+        }
+    }
+
+    private fun renderOCRResult(ocrResult: String) {
+        var recognizedText = ocrResult
+
+        // Put recognized text into the EditText content
+        Log.v(TAG, "OCR result: $recognizedText")
+        if (lang.equals("eng", ignoreCase = true)) {
+            recognizedText = recognizedText.replace("[^a-zA-Z0-9]+".toRegex(), " ")
+        }
+        recognizedText = recognizedText.trim { it <= ' ' }
+        if (recognizedText.isNotEmpty()) {
+            ocrResultEditText.setText(
+                if (ocrResultEditText.text.toString()
+                        .isEmpty()
+                ) recognizedText else ocrResultEditText.text.toString() + " " + recognizedText
+            )
+            ocrResultEditText.setSelection(ocrResultEditText.text.toString().length)
         }
     }
 }
